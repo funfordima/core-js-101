f102ced6810cc27ca78ba82aa67bcf4efa30eb8b
funfordima <d.g.litvinov@gmail.com>
2020-12-08 23:39:03 +0200

    feat: implement task 4

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -609,10 +609,18 @@ function getElementByIndexes(arr, indexes) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
-}
+function swapHeadAndTail(arr) {
+  const { length } = arr;
+  const delimiter = Math.floor(length / 2);
+  const head = arr.slice(0, delimiter);
+  if (!(length % 2)) {
+    const tail = arr.slice(delimiter);
+    return [...tail, ...head];
+  }
 
+  const tail = arr.slice(delimiter + 1);
+  return [...tail, arr[delimiter], ...head];
+}
 
 module.exports = {
   findElement,
--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,8 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  return new Date(new Date(date).getFullYear(), 1, 29).getDate() === 29;
 }
 
 
@@ -73,10 +73,10 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const timespan = new Date(startDate).getTime() - new Date(endDate).getTime();
+  return new Date(-timespan).toISOString().slice(11, -1);
 }
-
 
 /**
  * Returns the angle (in radians) between the hands of an analog clock
@@ -94,8 +94,14 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const ourDate = new Date(date);
+  let angle = Math.abs(0.5 * (60 * ourDate.getUTCHours() - 11 * ourDate.getUTCMinutes()));
+  if (angle > 360) {
+    angle -= 360;
+  }
+
+  return angle > 180 ? ((360 - angle) * Math.PI) / 180 : (angle * Math.PI) / 180;
 }
 
 
